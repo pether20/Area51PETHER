13e10eae63294e5fa9c81c839d2c85e185682217
Pether <pether.te.z@hotmail.com>
2019-11-28 20:45:39 -0500

    primer commit

---
+++ Clases/textClass/textClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace textClass {
+    class Program {
+        static void Main(string[] args) {
+
+            string secretWord = "Zapato";
+            string letter = "";
+            int hp = 3;
+
+            //Console.WriteLine(secretWord.Length);
+            for(int i=0;i < secretWord.Length;i++) 
+                {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese una letra:");
+            letter = Console.ReadLine();
+            Console.WriteLine(">" + letter);
+
+            Console.ReadLine();
+
+        }
+    }
+}
