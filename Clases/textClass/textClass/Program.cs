1d553367616bb4af06d43e1815e4f0009cea0668
Pether <pether.te.z@hotmail.com>
2019-11-28 22:00:23 -0500

    arreglando codigo

--- Clases/textClass/textClass/Program.cs
+++ Clases/textClass/textClass/Program.cs
@@ -8,23 +8,58 @@ namespace textClass {
     class Program {
         static void Main(string[] args) {
 
-            string secretWord = "Zapato";
+            string secretWord = "zapato";
             string letter = "";
+            string tempWord = "";
+            string publicWord = "";
             int hp = 3;
 
             //Console.WriteLine(secretWord.Length);
             for(int i=0;i < secretWord.Length;i++) 
                 {
-                Console.Write("*");
+                tempWord += "";
             }
-            Console.WriteLine();
+            publicWord = tempWord;
+            Console.WriteLine(publicWord);
 
-            Console.WriteLine("Ingrese una letra:");
-            letter = Console.ReadLine();
-            Console.WriteLine(">" + letter);
+
+            while (hp > 0) {
+                Console.WriteLine("Ingrese una letra:");
+                letter = Console.ReadLine();
+                //Console.WriteLine(">" + letter);
+
+                tempWord = "";
+                for (int i = 0; i < secretWord.Length; i++) 
+                    {
+                    if (letter == "" + secretWord[i]) 
+                    {
+                        tempWord += letter;
+
+                    } 
+                    else {
+                        tempWord += publicWord[i];
+                         }
+                }
+
+                if (publicWord == tempWord) {
+                    hp--;
+                }
+                
+                Console.WriteLine(tempWord);
+                
+            }
 
             Console.ReadLine();
 
         }
     }
 }
+//foreach (char c in secretWord) {
+//  if (letter == "" + c) {
+//    tempWord += c;
+//Console.WriteLine(tempWord);
+// } else {
+//   tempWord += "*";
+//Console.WriteLine(tempWord);
+// }
+//}
