fe710001ce3bec80f4a1b2f497eca1885c2d7352
Pether <pether.te.z@hotmail.com>
2019-12-03 11:46:40 -0500

    hambur code

---
+++ Clases/ProyectoHambur/Proyecto Hamburguesas/Proyecto Hamburguesas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Hamburguesas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/////    Bienvenido a Glotones51    /////");
+            Console.WriteLine();
+            Console.WriteLine(" /\\     __");
+            Console.WriteLine("   \\ .-':::. ");
+            Console.WriteLine("    \\ ::::: |\\ ");
+            Console.WriteLine("   |,\\:::' /  \\ ");
+            Console.WriteLine("    `.:::-'    \\");
+            Console.WriteLine("      `-.       \\          ___");
+            Console.WriteLine("          `-.    |      .-'';:::.");
+            Console.WriteLine("             `-.-'     / ',''.;;;\\");
+            Console.WriteLine("                      |  ','','.''|");
+            Console.WriteLine("                      |\\  ' ,',' /'");
+            Console.WriteLine("                       `.`-.___.-;'");
+            Console.WriteLine("                         `--._.-'");
+            Console.WriteLine();
+
+            Console.WriteLine("Eliga un pedido:");
+            Console.WriteLine("1.Salchipapa");
+            Console.WriteLine("2.Hamburguesa");
+            Console.WriteLine("3.Pollo Broster");
+           
+            Console.ReadLine();
+        }
+    }
+}
