4529fb9b1672cdaef7d645115d628a7ecd5e0827
pether <pether.te.z@hotmail.com>
2019-12-10 22:26:04 -0500

    animaciones

---
+++ Clases/Animaciones/animation/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigidbody2D;
+
+    public float speed = 1f;
+    public Animator animator;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float h =  Input.GetAxisRaw("Horizontal")*Time.deltaTime;
+
+        //if (h != 0)
+        //    animator.SetBool("Walk", true);
+        //else
+        //    animator.SetBool("Walk", false);
+
+        animator.SetBool("Walk", (h != 0));
+
+        if (h > 0)
+            spriteRenderer.flipX = false;
+        else if(h< 0)
+            spriteRenderer.flipX = true;
+
+        transform.Translate(Vector3.right * h*speed);
+        
+    }
+
+    void FixedUpdate() {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            rigidbody2D.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+    }
+}
