243090f4222129bac2896a69df9e8536593bf485
pether <pether.te.z@hotmail.com>
2019-12-12 22:15:54 -0500

    salto afinado

--- Clases/Animaciones/animation/Assets/Scripts/Player.cs
+++ Clases/Animaciones/animation/Assets/Scripts/Player.cs
@@ -6,17 +6,19 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private SpriteRenderer spriteRenderer;
-    private Rigidbody2D rigidbody2D;
+    private Rigidbody2D rbody2D;
 
-    public float speed = 1f;
+    public float speed = 5f;
     public Animator animator;
+
+    public bool grounded { get { return RoundAbsoluteToZero (rbody2D.velocity.y) == 0f; } }
     
     
     // Start is called before the first frame update
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -30,19 +32,37 @@ public class Player : MonoBehaviour
         //    animator.SetBool("Walk", false);
 
         animator.SetBool("Walk", (h != 0));
+        animator.SetBool("Jump", !grounded);
+        animator.SetFloat("vertical", Mathf.Sign(rbody2D.velocity.y));
+  
 
-        if (h > 0)
-            spriteRenderer.flipX = false;
-        else if(h< 0)
-            spriteRenderer.flipX = true;
+        //if (h > 0)
+        //    spriteRenderer.flipX = false;
+        //else if(h< 0)
+        //    spriteRenderer.flipX = true;
+        if (h != 0) { spriteRenderer.flipX = (h < 0); }
+
 
         transform.Translate(Vector3.right * h*speed);
-        
-    }
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space)) {
+            rbody2D.AddForce(Vector2.up *5, ForceMode2D.Impulse);
+            
+        }
+
+    } 
+
+        float RoundAbsoluteToZero (float decimalValue) {
+            decimalValue = Mathf.Abs(decimalValue);
+            if(decimalValue <= 0.01f) {
+                decimalValue = 0;
+            }
+            return decimalValue;
+        }
+   
 
     void FixedUpdate() {
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            rigidbody2D.AddForce(Vector2.up * 2, ForceMode2D.Impulse);
+        
     }
 }
